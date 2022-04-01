7e97f8cb12fd8d44fa42e94834154586383c346d
Ryan Gosdin <rgosdin@gmail.com>
2022-04-01 00:52:14 -0400

    AppPrefs.csproj

---
+++ AppPrefs/AppDelegate.cs
@@ -0,0 +1,43 @@
+using Foundation;
+using UIKit;
+
+namespace AppPrefs
+{
+	/// <summary>
+	/// The UIApplicationDelegate for the application. This class is responsible for launching the
+	/// User Interface of the application, as well as listening (and optionally responding) to
+	/// application events from iOS.
+	/// </summary>
+	[Register ("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		// class-level declarations
+		NSObject observer;
+
+		public override UIWindow Window { get; set; }
+
+		public override void FinishedLaunching (UIApplication application)
+		{
+			Settings.LoadDefaultValues();
+			observer = NSNotificationCenter.DefaultCenter.AddObserver ((NSString)"NSUserDefaultsDidChangeNotification", DefaultsChanged);
+			DefaultsChanged (null);
+		}
+
+		/// <summary>
+		/// This method is called when the application is about to terminate. Save data, if needed.
+		/// </summary>
+		/// <seealso cref="DidEnterBackground"/>
+		public override void WillTerminate (UIApplication application)
+		{
+			if (observer != null) {
+				NSNotificationCenter.DefaultCenter.RemoveObserver (observer);
+				observer = null;
+			}
+		}
+
+		void DefaultsChanged (NSNotification obj)
+		{
+			Settings.SetupByPreferences ();
+		}
+	}
+}
---
+++ AppPrefs/README.md
@@ -0,0 +1,13 @@
+Application Preferences
+=======================
+
+An example on how to display application preferences in
+the "Settings" system application.
+
+Loosely based on Apple's AppPrefs example:
+http://developer.apple.com/library/ios/#samplecode/AppPrefs
+
+Authors
+-------
+
+Mike Krüger
